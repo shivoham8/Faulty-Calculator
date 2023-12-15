a63c783f69bc3d3424780447ffaba769e5f35960
Ajay Gupta <97246211+shivoham8@users.noreply.github.com>
2023-12-15 21:48:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-let a = parseInt(prompt("Enter first number"));
-let b = parseInt(prompt("Enter second number"));
+// let a = parseInt(prompt("Enter first number"));
+// let b = parseInt(prompt("Enter second number"));
 
-let add = a + b;
-let subtract = a - b;
-let divide = a / b;
-let multiply = a * b;
+// let add = a + b;
+// let subtract = a - b;
+// let divide = a / b;
+// let multiply = a * b;
 
-let c = (Math.random()) * 100;
+// let c = (Math.random()) * 100;
 
-if(c < 10) {
-    console.log("The sum of two numbers is " + subtract);
-    console.log("The difference between two numbers is " + divide);
-    console.log("The division of two numbers is " + multiply);
-    console.log("The product of two numbers is " + add);
+// if(c < 10) {
+//     console.log("The sum of two numbers is " + subtract);
+//     console.log("The difference between two numbers is " + divide);
+//     console.log("The division of two numbers is " + multiply);
+//     console.log("The product of two numbers is " + add);
+// } 
+//  else {
+//     console.log("The sum of two numbers is " + add);
+//     console.log("The difference between two numbers is " + subtract);
+//     console.log("The division of two numbers is " + divide);
+//     console.log("The product of two numbers is " + multiply);
+//  }
+
+let random = Math.random();
+
+let a = prompt("Enter first number");
+let b = prompt("Enter operation");
+let c = prompt("Enter second number");
+
+let obj = {
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "*"
+}
+
+if(random > 0.1) {
+    alert(`The result is ${eval(`${a} ${b} ${c}`)}`);
 } 
- else {
-    console.log("The sum of two numbers is " + add);
-    console.log("The difference between two numbers is " + subtract);
-    console.log("The division of two numbers is " + divide);
-    console.log("The product of two numbers is " + multiply);
- }
+
+else {
+    b = obj[b];
+    alert(`The result is ${eval(`${a} ${b} ${c}`)}`);
+}   
